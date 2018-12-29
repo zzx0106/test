5cbcd9d21dbd61190d2a3e329bc87c0733796721
1148218318@qq.com <zzxzcbm06192412>
2018-12-29 14:20:57 +0800

    删除无用文件

--- app.js
+++ app.js
@@ -13,26 +13,13 @@ App({
     console.log('options', options);
   },
   onLaunch: function onLaunch(options) {
-    var _this = this;
-
     console.log('sence', options); // 判断是否由分享进入小程序
 
     if (options.scene == 1007 || options.scene == 1008) {
       this.globalData.share = true;
     } else {
       this.globalData.share = false;
-    } //获取设备顶部窗口的高度（不同设备窗口高度不一样，根据这个来设置自定义导航栏的高度）
-    //这个最初我是在组件中获取，但是出现了一个问题，当第一次进入小程序时导航栏会把
-    //页面内容盖住一部分,当打开调试重新进入时就没有问题，这个问题弄得我是莫名其妙
-    //虽然最后解决了，但是花费了不少时间
-
-
-    wx.getSystemInfo({
-      success: function success(res) {
-        console.log('getSystemInfo', res);
-        _this.globalData.height = res.statusBarHeight;
-      }
-    });
+    }
   },
   onShow: function onShow(options) {
     console.log('sence onShow', options);
@@ -60,16 +47,6 @@ App({
   },
   $http: $http,
   Util: Util,
-  config: {
-    title_height: '64',
-    statusbarHeight: '24',
-    titleIcon_height: '32',
-    titleIcon_width: '87',
-    title_top: '24',
-    title_text: 'xxx',
-    // iphone X + 24
-    prefix: 24
-  },
   globalData: {
     share: false,
     // 分享默认为false
